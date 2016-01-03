1131f176d56500b3a752f14c2a68002af68841d7
jiita <zknsftk1msrk@gmail.com>
2016-01-04 03:35:45 +0900

    add shell script for ln

---
+++ set_symbolic_links.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+ln -s $HOME/.config/gitconfig/gitconfig $HOME/.gitconfig
+
+ln -s $HOME/.config/vim/vimrc $HOME/.vimrc
+ln -s $HOME/.config/vim/gvimrc $HOME/.gvimrc
+
+ln -s $HOME/.config/urxvt/Xdefaults $HOME/.Xdefaults
+
+ln -s $HOME/.config/xorg/xinitrc $HOME/.xinitrc
