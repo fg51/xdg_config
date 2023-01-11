bbce7160ff659c1977a5b659ad97878c760d1963
kflange <zknsftk1msrk@gmail.com>
2023-01-11 15:54:30 +0900

    update

--- nvim/lua/plugins/statusline.lua
+++ nvim/lua/plugins/statusline.lua
@@ -3,51 +3,94 @@ return {
     "nvim-lualine/lualine.nvim", -- statusline
     lazy = true,
     event = "VeryLazy",
-    dependencies = { "kyazdani42/nvim-web-devicons" }, -- file icons
-    config = function()
-      require("lualine").setup({
-        options = {
-          icons_enabled = true,
-          theme = "nord",
-          section_separators = { left = "", right = "" },
-          component_separators = { left = "", right = "" },
-          disabled_filetypes = {},
-        },
-        sections = {
-          lualine_a = { "mode" },
-          lualine_b = { "branch" },
-          lualine_c = {
-            {
-              "filename",
-              file_status = true, -- displays file status (readonly status, modified status)
-              path = 1, -- 0 = just filename, 1 = relative path, 2 = absolute path
-            },
+    dependencies = { "nvim-tree/nvim-web-devicons" }, -- file icons
+    opts = {
+      options = {
+        icons_enabled = true,
+        theme = "nord",
+        section_separators = { left = "", right = "" },
+        component_separators = { left = "", right = "" },
+        disabled_filetypes = {},
+      },
+      sections = {
+        lualine_a = { "mode" },
+        lualine_b = { "branch" },
+        lualine_c = {
+          {
+            "filename",
+            file_status = true, -- displays file status (readonly status, modified status)
+            path = 1, -- 0 = just filename, 1 = relative path, 2 = absolute path
           },
-          lualine_x = {
-            { "encoding" },
-            { "fileformat", icons_enabled = false },
-            { "filetype", icons_enabled = false },
-          },
-          lualine_y = { "progress" },
-          lualine_z = { "location" },
         },
-        inactive_sections = {
-          lualine_a = {},
-          lualine_b = {},
-          lualine_c = {
-            {
-              "filename",
-              file_status = true, -- displays file status (readonly status, modified status)
-              path = 1, -- 0 = just filename, 1 = relative path, 2 = absolute path
-            },
-          },
-          lualine_x = { "location" },
-          lualine_y = {},
-          lualine_z = {},
+        lualine_x = {
+          { "encoding" },
+          { "fileformat", icons_enabled = false },
+          { "filetype", icons_enabled = false },
         },
-        tabline = {},
-        extensions = { "fugitive" },
-      })
-    end,
+        lualine_y = { "progress" },
+        lualine_z = { "location" },
+      },
+      inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = {
+          {
+            "filename",
+            file_status = true, -- displays file status (readonly status, modified status)
+            path = 1, -- 0 = just filename, 1 = relative path, 2 = absolute path
+          },
+        },
+        lualine_x = { "location" },
+        lualine_y = {},
+        lualine_z = {},
+      },
+      tabline = {},
+      extensions = { "fugitive" },
+    },
+    --config = function()
+    --  require("lualine").setup({
+    --    options = {
+    --      icons_enabled = true,
+    --      theme = "nord",
+    --      section_separators = { left = "", right = "" },
+    --      component_separators = { left = "", right = "" },
+    --      disabled_filetypes = {},
+    --    },
+    --    sections = {
+    --      lualine_a = { "mode" },
+    --      lualine_b = { "branch" },
+    --      lualine_c = {
+    --        {
+    --          "filename",
+    --          file_status = true, -- displays file status (readonly status, modified status)
+    --          path = 1, -- 0 = just filename, 1 = relative path, 2 = absolute path
+    --        },
+    --      },
+    --      lualine_x = {
+    --        { "encoding" },
+    --        { "fileformat", icons_enabled = false },
+    --        { "filetype", icons_enabled = false },
+    --      },
+    --      lualine_y = { "progress" },
+    --      lualine_z = { "location" },
+    --    },
+    --    inactive_sections = {
+    --      lualine_a = {},
+    --      lualine_b = {},
+    --      lualine_c = {
+    --        {
+    --          "filename",
+    --          file_status = true, -- displays file status (readonly status, modified status)
+    --          path = 1, -- 0 = just filename, 1 = relative path, 2 = absolute path
+    --        },
+    --      },
+    --      lualine_x = { "location" },
+    --      lualine_y = {},
+    --      lualine_z = {},
+    --    },
+    --    tabline = {},
+    --    extensions = { "fugitive" },
+    --  })
+    --end,
   },
 }
--- nvim/lua/plugins/tools.lua
+++ nvim/lua/plugins/tools.lua
@@ -4,10 +4,10 @@ return {
     lazy = false,
   },
   {
-    'kylechui/nvim-surround', -- add/delete/change surrounding pairs
+    "kylechui/nvim-surround", -- add/delete/change surrounding pairs
     version = "*",
     lazy = true,
-    event = { 'VeryLazy' }, -- event = { 'VimEnter' },
+    event = { "VeryLazy" }, -- event = { 'VimEnter' },
     -- config = function()
     --   require 'nvim-surround'.setup({})
     -- end
@@ -16,7 +16,7 @@ return {
   --  { 'h1mesuke/vim-alignta', opt = true, cmd = 'Alignta' }
 
   {
-    'windwp/nvim-autopairs', -- autopair
+    "windwp/nvim-autopairs", -- autopair
     lazy = true,
     event = { "InsertEnter" },
     -- config = function()
@@ -24,32 +24,32 @@ return {
     -- end,
   },
   {
-    'windwp/nvim-ts-autotag', -- autotag for html
+    "windwp/nvim-ts-autotag", -- autotag for html
     event = { "InsertEnter" },
     config = function()
-      require 'nvim-ts-autotag'.setup({})
-    end
+      require("nvim-ts-autotag").setup({})
+    end,
   },
   {
-    'phaazon/hop.nvim',
-    version = 'v2',
+    "phaazon/hop.nvim",
+    version = "v2",
     lazy = true,
     -- event = "VeryLazy",
     keys = {
-      { "f", "<cmd>lua require'hop'.hint_char1()<cr>", },
-      { '<leader>hf', "<cmd>lua require'hop'.hint_char2()<cr>", },
+      { "f", "<cmd>lua require'hop'.hint_char1()<cr>" },
+      { "<leader>hf", "<cmd>lua require'hop'.hint_char2()<cr>" },
     },
     config = function()
-      require 'hop'.setup {
-        keys = 'etovxqpdygfblzhckisuran',
-      }
+      require("hop").setup({
+        keys = "etovxqpdygfblzhckisuran",
+      })
       -- vim.api.nvim_set_keymap('n', 'f', "<cmd>lua require'hop'.hint_char1()<cr>", {})
       -- vim.api.nvim_set_keymap('n', '<leader>hf', "<cmd>lua require'hop'.hint_char2()<cr>",
       --   { noremap = true, silent = false })
     end,
   },
   {
-    'stevearc/dressing.nvim', -- allow plugin authors to override them with improvements upon ghe default behavior
+    "stevearc/dressing.nvim", -- allow plugin authors to override them with improvements upon ghe default behavior
     lazy = true,
     event = "VeryLazy",
   },
