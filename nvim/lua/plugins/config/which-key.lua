e6ce7533256700f81988faa6609b8e511622d777
fg51 <zknsftk1msrk@gmail.com>
2024-08-05 20:39:35 +0900

    Updated: which-key.

--- nvim/lua/plugins/config/which-key.lua
+++ nvim/lua/plugins/config/which-key.lua
@@ -5,30 +5,46 @@ return function()
   }
 
   require("which-key").setup({
+    -- preset = "classic", "modern", "helix"
+    preset = "helix",
+    delay = vim.o.timeoutlen,
+    triggers = {
+      { "<auto>", mode = "nixso" },
+    },
     plugins = {
+      marks = true,
+      registers = true,
+      spelling = {
+        enabled = true,
+        suggestions = 20,
+      },
       presets = {
-        operators = false,
         motions = false,
-        text_objects = false,
-        windows = false,
-        nav = false,
+        operators = false,
+        text_objects = true,
+        windows = true,
+        nav = true,
         z = true,
         g = true,
       },
     },
 
-    icons = {
-      breadcrumb = icons.ui.Separator,
-      separator = icons.misc.Vbar,
-      group = icons.misc.Add,
-    },
+    --  icons = {
+    --    breadcrumb = icons.ui.Separator,
+    --    separator = icons.misc.Vbar,
+    --    group = icons.misc.Add,
+    --  },
 
-    window = {
+    --  window = {
+    win = {
       border = "none",
-      position = "bottom",
-      margin = { 1, 0, 1, 0 },
-      padding = { 1, 1, 1, 1 },
-      winblend = 0,
+      --    position = "bottom",
+      --    margin = { 1, 0, 1, 0 },
+      --    padding = { 1, 1, 1, 1 },
+      padding = { 1, 2 },
+      --    winblend = 0,
+      wo = { winblend = 0 },
     },
+    expand = 1,
   })
 end
